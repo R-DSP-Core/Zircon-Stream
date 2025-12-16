dc1be5cfb2542905994d5397b7b19ac6d1f73aab
luot <1035768203@qq.com>
2025-12-17 06:45:00 +0800

    stream:cfg_tilestride should use rk rj

--- src/main/scala/Frontend/Decode/PreDecoder.scala
+++ src/main/scala/Frontend/Decode/PreDecoder.scala
@@ -41,8 +41,8 @@ class PreDecoder extends Module {
     val isStream = inst(6,0)===0x0b.U//0001011
     val streamOp = inst(14,12) 
     val stRdVld = isStream && (streamOp === CALSTREAMRD)
-    val stRkVld = isStream && (streamOp === CFGI  || streamOp === CFGLOAD || streamOp === CFGSTORE || streamOp === CFGSTRIDE || streamOp === CFGREUSE)
-    val stRjVld = isStream && (streamOp === CFGI  || streamOp === CFGLOAD || streamOp === CFGSTORE || streamOp === CFGSTRIDE || streamOp === CFGREUSE)
+    val stRkVld = isStream && (streamOp === CFGI  || streamOp === CFGLOAD || streamOp === CFGSTORE || streamOp === CFGSTRIDE || streamOp === CFGREUSE || streamOp === CFGTILESTRIDE )
+    val stRjVld = isStream && (streamOp === CFGI  || streamOp === CFGLOAD || streamOp === CFGSTORE || streamOp === CFGSTRIDE || streamOp === CFGREUSE || streamOp === CFGTILESTRIDE )
 
     // rd
     val rdVld = (inst(3, 0) === 0x3.U && !(
